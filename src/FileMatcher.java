a5af3d1101ac0bdcd2f919643d71971e0ee470f1
Kshitij Karthick <kshitij.karthick@gmail.com>
2014-11-28 23:29:44 +0530

    Ends With Pattern Checking Functionality Added

---
+++ src/FileMatcher.java
@@ -0,0 +1,34 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+/**
+ *
+ * @author kshitijkarthick
+ */
+import java.io.File;
+import java.util.LinkedList;
+public class FileMatcher {
+    private String path;
+    public void FileMatcher(){
+        setPath(".");
+    }
+    public void FileMatcher(String path){
+        setPath(path);
+    }
+    public void setPath(String path){
+        this.path=path;
+    }
+    public File[] ListFiles(){
+        return this.ListFiles();
+    }
+    public LinkedList endsWithPattern(String pattern){
+        LinkedList list=new LinkedList();
+        for(File filename:ListFiles()){
+            if(filename.getName().endsWith(pattern))
+                list.add(filename);
+        }
+        return list;
+    }
+}
